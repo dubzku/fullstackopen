bb5ceaf955a1ef304f1042a62095faa12b8bda44
Winnie <winn.ku@gmail.com>
2020-12-10 15:08:24 -0500

    finished part1-c

--- part1/part1-c/src/index.js
+++ part1/part1-c/src/index.js
@@ -2,19 +2,11 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Display = (props) => {
-  return (
-    <div>{props.counter}</div>
-  )
-}
+const Display = ( { counter } ) => <div>{counter}</div> 
 
-const Button = (props) => {
-  return (
-    <button onClick={ props.handleClick }>
-      {props.text}
-    </button>
-  )
-}
+const Button = ({ handleClick, text}) => <button onClick={ handleClick }>{text}</button>
+  
+
 
 const App = () => {
   // function call usestate(0) adds state to the component - initializes it with the value of 0
@@ -44,6 +36,13 @@ ReactDOM.render(
 <App />, 
   document.getElementById('root')
 )
+
+// Explanation:
+// When the application starts, the code in App is executed. This code uses a useState hook to create the application state, setting an initial value of the variable counter. This component contains the Display component - which displays the counter's value, 0 - and three Button components. The buttons all have event handlers, which are used to change the state of the counter.
+
+// When one of the buttons is clicked, the event handler is executed. The event handler changes the state of the App component with the setCounter function. Calling a function which changes the state causes the component to rerender.
+
+// So, if a user clicks the plus button, the button's event handler changes the value of counter to 1, and the App component is rerendered. This causes its subcomponents Display and Button to also be re-rendered. Display receives the new value of the counter, 1, as props. The Button components receive event handlers which can be used to change the state of the counter.
 
 
 
