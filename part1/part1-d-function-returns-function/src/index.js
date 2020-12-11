06d372caef46be2a1e7df1e782cd94f4f074dd6c
Winnie <winn.ku@gmail.com>
2020-12-11 16:41:16 -0500

    finished part1-d

--- part1/part1-d-function-returns-function/src/index.js
+++ part1/part1-d-function-returns-function/src/index.js
@@ -1,6 +1,14 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const Display = props => <div>{props.value}</div>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
 const App = () => {
   const [ value, setValue ] = useState(10)
 
@@ -8,14 +16,13 @@ const App = () => {
   const setToValue = (newValue) => {
     setValue(newValue)
   } 
-  
 
   return (
     <div>
-      {value}
-      <button onClick={ () => setToValue(1000) }>thousand</button>
-      <button onClick={ () => setToValue(0) }>reset</button>
-      <button onClick={ () => setToValue(value + 1) }>increment</button>
+      <Display value={ value } />
+      <Button handleClick={ () => setToValue(1000) } text="thousand" />
+      <Button handleClick={ () => setToValue(0) } text="reset" />
+      <Button handleClick={ () => setToValue(value + 1) } text="increment" />
     </div>
   )
 }
